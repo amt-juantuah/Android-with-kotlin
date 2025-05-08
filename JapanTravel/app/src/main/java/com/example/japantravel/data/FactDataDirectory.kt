ed98e36942611c628a122fe0af77b3e2cc0f5ca1
Richard Osei <richardosei@richards-mbp.home>
2025-05-09 00:46:08 +0100

    project: Japan Facts

---
+++ JapanTravel/app/src/main/java/com/example/japantravel/data/FactDataDirectory.kt
@@ -0,0 +1,111 @@
+package com.example.japantravel.data
+
+import com.example.japantravel.R
+import com.example.japantravel.model.JapanFact
+
+object FactDataDirectory {
+    val japanFacts: List<JapanFact> = listOf(
+        JapanFact(
+            dayRes = R.string.day_1,
+            imageRes = R.drawable.f1,
+            factRes = R.string.title_1,
+            factDetailsRes = R.string.fact_1
+        ),
+        JapanFact(
+            dayRes = R.string.day_2,
+            imageRes = R.drawable.f2,
+            factRes = R.string.title_2,
+            factDetailsRes = R.string.fact_2
+        ),
+        JapanFact(
+            dayRes = R.string.day_3,
+            imageRes = R.drawable.f3,
+            factRes = R.string.title_3,
+            factDetailsRes = R.string.fact_3
+        ),
+        JapanFact(
+            dayRes = R.string.day_4,
+            imageRes = R.drawable.f4,
+            factRes = R.string.title_4,
+            factDetailsRes = R.string.fact_4
+        ),
+        JapanFact(
+            dayRes = R.string.day_5,
+            imageRes = R.drawable.f5,
+            factRes = R.string.title_5,
+            factDetailsRes = R.string.fact_5
+        ),
+        JapanFact(
+            dayRes = R.string.day_6,
+            imageRes = R.drawable.f6,
+            factRes = R.string.title_6,
+            factDetailsRes = R.string.fact_6
+        ),
+        JapanFact(
+            dayRes = R.string.day_7,
+            imageRes = R.drawable.f7,
+            factRes = R.string.title_7,
+            factDetailsRes = R.string.fact_7
+        ),
+        JapanFact(
+            dayRes = R.string.day_8,
+            imageRes = R.drawable.f8,
+            factRes = R.string.title_8,
+            factDetailsRes = R.string.fact_8
+        ),
+        JapanFact(
+            dayRes = R.string.day_9,
+            imageRes = R.drawable.f9,
+            factRes = R.string.title_9,
+            factDetailsRes = R.string.fact_9
+        ),
+        JapanFact(
+            dayRes = R.string.day_10,
+            imageRes = R.drawable.f10,
+            factRes = R.string.title_10,
+            factDetailsRes = R.string.fact_10
+        ),
+        JapanFact(
+            dayRes = R.string.day_11,
+            imageRes = R.drawable.f11,
+            factRes = R.string.title_11,
+            factDetailsRes = R.string.fact_11
+        ),
+        JapanFact(
+            dayRes = R.string.day_12,
+            imageRes = R.drawable.f12,
+            factRes = R.string.title_12,
+            factDetailsRes = R.string.fact_12
+        ),
+        JapanFact(
+            dayRes = R.string.day_13,
+            imageRes = R.drawable.f13,
+            factRes = R.string.title_13,
+            factDetailsRes = R.string.fact_13
+        ),
+        JapanFact(
+            dayRes = R.string.day_14,
+            imageRes = R.drawable.f14,
+            factRes = R.string.title_14,
+            factDetailsRes = R.string.fact_14
+        ),
+        JapanFact(
+            dayRes = R.string.day_15,
+            imageRes = R.drawable.f15,
+            factRes = R.string.title_15,
+            factDetailsRes = R.string.fact_15
+        ),
+        JapanFact(
+            dayRes = R.string.day_16,
+            imageRes = R.drawable.f16,
+            factRes = R.string.title_16,
+            factDetailsRes = R.string.fact_16
+        ),
+        JapanFact(
+            dayRes = R.string.day_17,
+            imageRes = R.drawable.f17,
+            factRes = R.string.title_17,
+            factDetailsRes = R.string.fact_17
+        ),
+        )
+}
---
+++ Japanfacts/app/src/main/java/com/example/japanfacts/model/Fact.kt
@@ -0,0 +1,3 @@
+package com.example.japanfacts.model
+
+data class Fact()
